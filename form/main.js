848afb785489f7a8982409087b8bdf5932493567
MordarskiK <102521828+MordarskiK@users.noreply.github.com>
2023-10-10 12:55:47 +0200

    done

--- form/main.js
+++ form/main.js
@@ -1,3 +1,4 @@
+const list = document.querySelector("#list");
 const num1 = document.querySelector("#num1");
 const num2 = document.querySelector("#num2");
 const form = document.querySelector("form");
@@ -5,5 +6,17 @@ const display = document.querySelector("#display");
 
 form.addEventListener("submit", function (evt) {
   evt.preventDefault();
-  display.innerHTML = parseInt(num1.value) + parseInt(num2.value);
+
+  if(list.value == 1){
+    display.innerHTML = parseInt(num1.value) + parseInt(num2.value);
+  }
+  if(list.value == 2){
+    display.innerHTML = parseInt(num1.value) - parseInt(num2.value);
+  }
+  if(list.value == 3){
+    display.innerHTML = parseInt(num1.value) * parseInt(num2.value);
+  }
+  if(list.value == 4){
+    display.innerHTML = parseInt(num1.value) / parseInt(num2.value);
+  }
 });
